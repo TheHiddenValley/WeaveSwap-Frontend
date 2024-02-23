61b0f05912c9796a8fcd5e51cda4bb0ca0ec17a8
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2024-02-24 00:11:31 +0800

    Create lend.tsx

---
+++ pages/lend.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import SwapContainer from "@/containers/swapConatiner";
+export default function lend() {
+  return (
+    <div className={`container mx-auto`}>
+      <SwapContainer />
+    </div>
+  );
+}
