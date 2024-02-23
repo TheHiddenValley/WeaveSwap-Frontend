06b650fc88ec320f301504bac61d343f26112362
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2024-02-24 03:29:15 +0800

    Update index.tsx

--- containers/swapConatiner/index.tsx
+++ containers/swapConatiner/index.tsx
@@ -10,11 +10,12 @@ import SecondNetworkModal from "@/components/modal/secondNetworkModal";
 import CoinPrice from "@/components/coinPrice/Coinprice";
 
 
-const OneSVG = () => <Image src="/recent 1.svg" alt="One SVG" width={16} height={16} />;
-const Cog6ToothSVG = () => <Image src="/cog-6-tooth.svg" alt="Cog 6 Tooth SVG" width={16} height={16} />;
+const OneSVG = () => <Image src="/recent 1.svg" alt="One SVG" width={30} height={30} />;
+const Cog6ToothSVG = () => <Image src="/cog-6-tooth.svg" alt="Cog 6 Tooth SVG" width={20} height={20} />;
+
 const ArrowsRightLeft = () => (
   <button type="button" onClick={handleClick} style={{ width: '100%', display: 'flex', justifyContent: 'space-evenly' }}>
-    <Image src="/arrows-right-left.svg" alt="Cog 6 Tooth SVG" width={16} height={16} />
+    <Image src="/arrows-right-left.svg" alt="Cog 6 Tooth SVG" width={30} height={30} />
   </button>
 );
 const handleClick = () => {
