a4e2e0d4a54d387d6c7ba994191c213488be7e08
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2024-02-24 03:20:38 +0800

    Update index.tsx

--- containers/swapConatiner/index.tsx
+++ containers/swapConatiner/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./index.module.css";
 import Image from 'next/image';
-import SingleCrossSwapInput from "@/components/inputs/singleCrossPayment/SingleCrossSwapInput";
+import SingleCrossSwapInput from "@/components/inputs/singleCrossPayment/singleCrossSwapInput";
 import SingleCrossSwapOutput from "@/components/inputs/singleCrossPayment/SingleCrossSwapOutput";
 import ConnectWalletPage from "@/components/connectWalletPage";
 import { useAddress } from "@thirdweb-dev/react";
