20bac290bc83f6f9fbdf38541ab615af7d29d5a9
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2024-02-24 00:11:05 +0800

    Update pool.tsx

--- pages/pool.tsx
+++ pages/pool.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SwapContainer from "@/containers/swapConatiner";
-export default function Swap() {
+export default function pool() {
   return (
     <div className={`container mx-auto`}>
       <SwapContainer />
