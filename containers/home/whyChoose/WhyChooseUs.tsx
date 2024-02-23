99c9fe51d0099ae973e0c1ccffc72f9b019979c9
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2024-02-23 22:48:20 +0800

    Update WhyChooseUs.tsx

--- containers/home/whyChoose/WhyChooseUs.tsx
+++ containers/home/whyChoose/WhyChooseUs.tsx
@@ -5,32 +5,15 @@ import CloudPng from "../../../assets/png/blockchain cloud.png";
 import styles from "./whyChoose.module.css";
 export default function WhyChooseUs() {
   return (
-    <div className={`${styles.WhyChooseUs} container mx-auto`}>
-      <h1 className={styles.Empowered}>Empowered by the community</h1>
-      <section className={styles.WhyChooseUs_section}>
-        <div className={styles.WhyChooseUs_div}>
-          <div>
-            <h1>Security First</h1>
-            <p>
-              Our smart contracts prioritize the security of your digital
-              assets, implementing state-of-the-art measures to safeguard your
-              transactions.
-            </p>
-          </div>
-          <Image src={SecurityPng} alt="" width={500} height={500} />
-        </div>{" "}
-        <div className={styles.WhyChooseUs_div}>
-          <div>
-            <h1>Security First</h1>
-            <p>
-              Our smart contracts prioritize the security of your digital
-              assets, implementing state-of-the-art measures to safeguard your
-              transactions.
-            </p>
-          </div>
-          <Image src={CloudPng} alt="" width={800} height={500} />
-        </div>
-      </section>
-    </div>
+<div style={{ width: '100%', height: '100%', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', gap: 40, display: 'inline-flex', marginTop: '5em' }}>
+<div style={{color: 'white', fontSize: 40, fontFamily: 'Hubot-Sans', fontWeight: '700', wordWrap: 'break-word'}}>Get on our token list</div>
+<div style={{width: 954, textAlign: 'center', color: '#8E8E8E', fontSize: 20, fontFamily: 'Hubot-Sans', fontWeight: '500', lineHeight: 24, wordWrap: 'break-word'}}>Join us in our mission to revolutionize decentralized finance while building trust and credibility.Projects that wish to have their token show in the default swap list will need to submit their information via our partner portal. Otherwise users can search for any supported token via the contract address.</div>
+<div style={{paddingLeft: 16, paddingRight: 16, paddingTop: 10, paddingBottom: 10, background: 'white', borderRadius: 10, justifyContent: 'center', alignItems: 'center', gap: 12, display: 'inline-flex'}}>
+<div style={{textAlign: 'center', color: 'black', fontSize: 18, fontFamily: 'Hubot-Sans', fontWeight: '500', wordWrap: 'break-word'}}>Apply now</div>
+<div style={{width: 24, height: 24, position: 'relative'}}>
+<div style={{width: 19.50, height: 19.50, left: 2.25, top: 2.25, position: 'absolute', background: 'black'}}></div>
+</div>
+</div>
+</div>
   );
 }
