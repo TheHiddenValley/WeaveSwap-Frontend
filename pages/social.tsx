89ddb4b4f7b1ba54c491310d59b041d349eb3369
The Hidden Valley <108282277+TheHiddenValley@users.noreply.github.com>
2024-02-24 00:23:12 +0800

    Create social.tsx

---
+++ pages/social.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import JoinUs from "@/components/joinUs/JoinUs";
+
+export default function lend() {
+  return (
+    <div className={`container mx-auto`}>
+      <JoinUs />
+    </div>
+  );
+}
